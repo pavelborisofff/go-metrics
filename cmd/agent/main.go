25f7c6fc26d593eaab885cd1a554d760a68cbdd5
pavelborisofff <pavel.borisofff@gmail.com>
2023-10-08 22:24:21 +0300

    iter 2 - close post response body

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"math/rand"
 	"net/http"
@@ -132,6 +133,13 @@ func (s *MemStorage) SendMetric(metricType string, metricName string, metricValu
 
 	msg := fmt.Sprintf("Metric sent successfully: %s", url)
 	log.Default().Println(msg)
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(res.Body)
 }
 
 func main() {
