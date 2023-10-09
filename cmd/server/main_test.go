49179c55a77121992caaac0175d15f7e6988e9b1
pavelborisofff <pavel.borisofff@gmail.com>
2023-10-09 15:03:51 +0300

    iter 5 - fix tests for server

--- cmd/server/main_test.go
+++ cmd/server/main_test.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
+
+func testRequest(t *testing.T, ts *httptest.Server, method, path string) (*http.Response, string) {
+	req, err := http.NewRequest(method, ts.URL+path, nil)
+	require.NoError(t, err)
+
+	resp, err := ts.Client().Do(req)
+	require.NoError(t, err)
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
+
+	return resp, string(respBody)
+}
 
 func TestMainHandler(t *testing.T) {
 	type testType struct {
@@ -39,131 +57,148 @@ func TestMainHandler(t *testing.T) {
 	}
 }
 
-//func TestUpdateHandler(t *testing.T) {
-//	type testType struct {
-//		name         string
-//		method       string
-//		requestURL   string
-//		expectedCode int
-//		expectedBody string
-//	}
-//
-//	tests := []testType{
-//		{
-//			name:         "Receive 405 if method is not POST",
-//			method:       http.MethodGet,
-//			requestURL:   "/update/",
-//			expectedCode: http.StatusBadRequest,
-//			expectedBody: "",
-//		},
-//		{
-//			name:         "Update Counter",
-//			method:       http.MethodPost,
-//			requestURL:   "/update/counter/anyCounter/5",
-//			expectedCode: http.StatusOK,
-//			expectedBody: "",
-//		},
-//		{
-//			name:         "Update Counter with invalid value",
-//			method:       http.MethodPost,
-//			requestURL:   "/update/counter/anyCounter/invalid",
-//			expectedCode: http.StatusBadRequest,
-//			expectedBody: "Bad Counter's value: invalid\n",
-//		},
-//		{
-//			name:         "Update Gauge",
-//			method:       http.MethodPost,
-//			requestURL:   "/update/gauge/anyGauge/123.123123",
-//			expectedCode: http.StatusOK,
-//			expectedBody: "",
-//		},
-//		{
-//			name:         "Update Gauge with invalid value",
-//			method:       http.MethodPost,
-//			requestURL:   "/update/gauge/anyGauge/invalid",
-//			expectedCode: http.StatusBadRequest,
-//			expectedBody: "Bad metric's value: anyGauge invalid\n",
-//		},
-//	}
-//
-//	for _, test := range tests {
-//		t.Run(test.name, func(t *testing.T) {
-//			storage := NewMemStorage()
-//			r := httptest.NewRequest(test.method, test.requestURL, nil)
-//			w := httptest.NewRecorder()
-//
-//			updateHandler(w, r, storage)
-//
-//			assert.Equal(t, test.expectedCode, w.Code, "Status code mismatch")
-//
-//			if test.expectedBody != "" {
-//				assert.Equal(t, test.expectedBody, w.Body.String(), "Response body mismatch")
-//			}
-//		})
-//	}
-//}
+func TestUpdateHandler(t *testing.T) {
+	type testType struct {
+		name         string
+		method       string
+		requestURL   string
+		expectedCode int
+		expectedBody string
+	}
 
-//func TestMetricsHandler(t *testing.T) {
-//	type testType struct {
-//		name         string
-//		method       string
-//		requestURL   string
-//		expectedCode int
-//		expectedBody string
-//	}
-//
-//	tests := []testType{
-//		{
-//			name:         "Receive 405 if method is not GET",
-//			method:       http.MethodPost,
-//			requestURL:   "/metrics",
-//			expectedCode: http.StatusMethodNotAllowed,
-//			expectedBody: "",
-//		},
-//		{
-//			name:         "Get Metrics",
-//			method:       http.MethodGet,
-//			requestURL:   "/metrics/",
-//			expectedCode: http.StatusOK,
-//			expectedBody: `{"counter":{},"gauge":{}}`,
-//		},
-//		{
-//			name:         "Get Metrics with data",
-//			method:       http.MethodGet,
-//			requestURL:   "/metrics/",
-//			expectedCode: http.StatusOK,
-//			expectedBody: `{"counter":{},"gauge":{"anygauge":123.123123}}`,
-//		},
-//	}
-//
-//	storage := NewMemStorage()
-//
-//	for _, test := range tests[:len(tests)-1] {
-//		t.Run(test.name, func(t *testing.T) {
-//			r := httptest.NewRequest(test.method, test.requestURL, nil)
-//			w := httptest.NewRecorder()
-//
-//			metricsHandler(w, r, storage)
-//
-//			assert.Equal(t, test.expectedCode, w.Code, "Status code mismatch")
-//
-//			if test.expectedBody != "" {
-//				assert.Equal(t, test.expectedBody, w.Body.String(), "Response body mismatch")
-//			}
-//		})
-//	}
-//
-//	t.Run(tests[len(tests)-1].name, func(t *testing.T) {
-//		storage.UpdateGauge("anygauge", 123.123123)
-//		r := httptest.NewRequest(tests[len(tests)-1].method, tests[len(tests)-1].requestURL, nil)
-//		w := httptest.NewRecorder()
-//
-//		metricsHandler(w, r, storage)
-//
-//		assert.Equal(t, tests[len(tests)-1].expectedCode, w.Code, "Status code mismatch")
-//
-//		if tests[len(tests)-1].expectedBody != "" {
-//			assert.Equal(t, tests[len(tests)-1].expectedBody, w.Body.String(), "Response body mismatch")
-//		}
-//	})
-//}
+	tests := []testType{
+		{
+			name:         "Receive 404 if method is not POST",
+			method:       http.MethodGet,
+			requestURL:   "/update/",
+			expectedCode: http.StatusNotFound,
+			expectedBody: "404 page not found\n",
+		},
+		{
+			name:         "Update Counter",
+			method:       http.MethodPost,
+			requestURL:   "/update/counter/anyCounter/5",
+			expectedCode: http.StatusOK,
+			expectedBody: "",
+		},
+		{
+			name:         "Update Counter with invalid value",
+			method:       http.MethodPost,
+			requestURL:   "/update/counter/anyCounter/invalid",
+			expectedCode: http.StatusBadRequest,
+			expectedBody: "Bad Counter's value: invalid\n",
+		},
+		{
+			name:         "Update Gauge",
+			method:       http.MethodPost,
+			requestURL:   "/update/gauge/anyGauge/123.123123",
+			expectedCode: http.StatusOK,
+			expectedBody: "",
+		},
+		{
+			name:         "Update Gauge with invalid value",
+			method:       http.MethodPost,
+			requestURL:   "/update/gauge/anyGauge/invalid",
+			expectedCode: http.StatusBadRequest,
+			expectedBody: "Bad metric's value: anyGauge invalid\n",
+		},
+	}
+
+	storage := NewMemStorage()
+	r := chi.NewRouter()
+
+	r.Get("/", func(res http.ResponseWriter, req *http.Request) {
+		mainHandler(res, req, storage)
+	})
+	r.Post("/update/{metric-type}/{metric-name}/{metric-value}", func(res http.ResponseWriter, req *http.Request) {
+		updateHandler(res, req, storage)
+	})
+	r.Get("/value/{metric-type}/{metric-name}", func(res http.ResponseWriter, req *http.Request) {
+		valueHandler(res, req, storage)
+	})
+	r.Get("/metrics", func(res http.ResponseWriter, req *http.Request) {
+		metricsHandler(res, req, storage)
+	})
+
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	for _, test := range tests {
+		fmt.Println(test.name)
+		fmt.Println(test.requestURL)
+		res, body := testRequest(t, ts, test.method, test.requestURL)
+		assert.Equal(t, test.expectedCode, res.StatusCode)
+		assert.Equal(t, test.expectedBody, body)
+	}
+}
+
+func TestMetricsHandler(t *testing.T) {
+	type testType struct {
+		name         string
+		method       string
+		requestURL   string
+		expectedCode int
+		expectedBody string
+	}
+
+	tests := []testType{
+		{
+			name:         "Receive 405 if method is not GET",
+			method:       http.MethodPost,
+			requestURL:   "/metrics",
+			expectedCode: http.StatusMethodNotAllowed,
+			expectedBody: "",
+		},
+		{
+			name:         "Get Metrics",
+			method:       http.MethodGet,
+			requestURL:   "/metrics",
+			expectedCode: http.StatusOK,
+			expectedBody: `{"counter":{},"gauge":{}}`,
+		},
+		{
+			name:         "Get Metrics with data",
+			method:       http.MethodGet,
+			requestURL:   "/metrics",
+			expectedCode: http.StatusOK,
+			expectedBody: `{"counter":{},"gauge":{"anygauge":123.123123}}`,
+		},
+	}
+
+	storage := NewMemStorage()
+	r := chi.NewRouter()
+
+	r.Get("/", func(res http.ResponseWriter, req *http.Request) {
+		mainHandler(res, req, storage)
+	})
+	r.Post("/update/{metric-type}/{metric-name}/{metric-value}", func(res http.ResponseWriter, req *http.Request) {
+		updateHandler(res, req, storage)
+	})
+	r.Get("/value/{metric-type}/{metric-name}", func(res http.ResponseWriter, req *http.Request) {
+		valueHandler(res, req, storage)
+	})
+	r.Get("/metrics", func(res http.ResponseWriter, req *http.Request) {
+		metricsHandler(res, req, storage)
+	})
+
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	for _, test := range tests[:len(tests)-1] {
+		fmt.Println(test.name)
+		fmt.Println(test.requestURL)
+		res, body := testRequest(t, ts, test.method, test.requestURL)
+		assert.Equal(t, test.expectedCode, res.StatusCode)
+		assert.Equal(t, test.expectedBody, body)
+	}
+
+	for _, test := range tests[len(tests)-1:] {
+		storage.UpdateGauge("anygauge", 123.123123)
+		fmt.Println(test.name)
+		fmt.Println(test.requestURL)
+		res, body := testRequest(t, ts, test.method, test.requestURL)
+		assert.Equal(t, test.expectedCode, res.StatusCode)
+		assert.Equal(t, test.expectedBody, body)
+	}
+
+}
